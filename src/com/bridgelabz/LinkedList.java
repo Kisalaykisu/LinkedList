993a5f363efb054f4c29b1c19d071c1445ada46c
Kisalay <kisalaykisu@gmail.com>
2022-06-30 18:10:16 +0530

    Welcome message for linkedlist

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,8 @@
+// Welcome Message
+package com.bridgelabz;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the LinkedList Program");
+    }
+}
