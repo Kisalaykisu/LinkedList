f09fd3206838701d7a1ef0fb53f9ddf671eba532
Kisalay <kisalaykisu@gmail.com>
2022-06-30 18:54:34 +0530

    UC3 has been added

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -6,7 +6,7 @@ package com.bridgelabz;
 
 public class LinkedList {
     Node head;  // Taking node as head
-    Node tail;  // Taking node as head
+    Node tail;    // Taking node as head
 
     /**
      * method to create Linked List
@@ -17,12 +17,31 @@ public class LinkedList {
     public Node push(int data) { //This is Node class
         Node newNode = new Node(data); //Create a new node
         if (head == null) { //Checks if the list is empty
-            this.head = newNode; //If list is empty, both head and tail will point to new node
+            this.head = newNode;  //If list is empty, both head and tail will point to new node
             this.tail = newNode;
         } else {
             Node temp = head; //Node current will point to head
             this.head = newNode;
             newNode.next = temp;
+        }
+        return newNode;
+    }
+
+    /**
+     * method to create a Linked List
+     * append method is used to add data 1st in 1st out
+     *
+     * @param data integer to be added in linked list
+     */
+    public Node append(int data) {  // append used
+        Node newNode = new Node(data);
+        if (head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            Node temp = tail;
+            this.tail = newNode;
+            temp.next = newNode;
         }
         return newNode;
     }
@@ -42,3 +61,4 @@ public class LinkedList {
         }
     }
 }
+
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,11 +2,11 @@ package com.bridgelabz;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        LinkedList obj = new LinkedList(); //Make LinkedList obj
-        obj.push(70);
-        obj.push(30);
-        obj.push(56);
-        System.out.println("The current LinkedList is: ");
+        LinkedList obj = new LinkedList();
+        obj.append(56);
+        obj.append(30);
+        obj.append(70);
+        System.out.println("The current LinkedList using append method is: ");
         obj.showLinkedList();
     }
 }
